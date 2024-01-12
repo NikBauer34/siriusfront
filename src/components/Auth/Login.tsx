f2a73a0f058ccea4db74b32e9f9f2852d4bd92b6
NikBauer34 <nik.bauer.09@mail.ri>
2024-01-12 14:46:12 +0300

    feature/apikey

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react'
 import { LoginForm } from '../../modules/components/index'
 import { SchemeToggle } from '../../ui'
+import { YMap } from '..'
 const Login: FC = () => {
     return (
         <div>
             <LoginForm></LoginForm>
-            <SchemeToggle/>
         </div>
     )
 }
