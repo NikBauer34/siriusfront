2b4c196d77b129a83dc46fca841c3d9ee29b529d
NikBauer34 <nik.bauer.09@mail.ri>
2024-01-12 14:31:46 +0300

    feature/scheme-toggle

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
 import { LoginForm } from '../../modules/components/index'
+import { SchemeToggle } from '../../ui'
 const Login: FC = () => {
     return (
         <div>
             <LoginForm></LoginForm>
+            <SchemeToggle/>
         </div>
     )
 }
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,8 +1,10 @@
 import UnderlineInput from "./UnderlineInput/UnderlineInput";
 import RouteSVG from "./RouteSVG";
 import InputBorder from "./InputBorder/InputBorder";
+import SchemeToggle from "./SchemeToggle/SchemeToggle";
 export {
     UnderlineInput,
     RouteSVG,
-    InputBorder
+    InputBorder,
+    SchemeToggle
 }
