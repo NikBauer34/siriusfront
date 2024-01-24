4a872e923d0470e6f10a7cb4fc65fa004bb17815
NikBauer34 <nik.bauer.09@mail.ri>
2024-01-24 12:14:28 +0300

    feature/formdata-works

--- src/pages/FormDataTest.tsx
+++ src/pages/FormDataTest.tsx
@@ -7,15 +7,15 @@ const FormDataTest: FC = () => {
     const formdata = new FormData()
     const {magnetogram} = useContext(Context)
     const onClick = async() => {
-        formdata.append('pipe_id', '65aba801804ec013face41b3')
-        formdata.append('version', '1.0.0')
-        formdata.append('title', 'Нью магнитограмма')
+        formdata.append("pipe_id", "65aba801804ec013face41b3")
+        formdata.append("version", "1.0.0")
+        formdata.append("title", "Нью магнитограмма")
         const response = await magnetogram.createMagnetogram(formdata)
         console.log(response)
     }
     return (
         <>
-            <Dropzone maxSize={5 * 1024 ** 2} onDrop={(files) => formdata.append('file', files[0])}></Dropzone>
+            <Dropzone maxSize={5 * 1024 ** 2} onDrop={(files) => formdata.append("file", files[0])}></Dropzone>
             <Button onClick={onClick}>Click me</Button>
         </>
     )
